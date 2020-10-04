187b4500a6338bff86961e0962f94e072ed44ccb
Gurpreet1009 <71648864+Gurpreet1009@users.noreply.github.com>
2020-10-04 22:07:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# coding: utf-8
+
+
+# In[ ]:
+
+
+
+
+
+from flask import Flask ,render_template,request
+
+app=Flask(__name__)
+
+@app.route("/")
+
+def xyz():
+
+    return render_template("power.html")
+
+@app.route("/userinput",methods=['GET','POST'])
+
+def abc():
+
+    if(request.method=='POST'):
+        num=int(request.form['n'])
+
+        power=int(request.form['p'])
+
+        r=(num**power)
+
+        return render_template("power.html",result=r)
+
+if __name__=="__main__":
+
+    app.run()
+
+
+
+
+
+
+
